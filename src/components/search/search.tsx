20a792fd3f5f7f13a26b500329985eaa2548baf0
VadimLitau <vadimlitau@yandex.ru>
2024-03-13 16:27:39 +0500

    добавил кастомный хук для инпутов

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,10 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import styles from "./search.module.css";
+import useForm from "../../hooks/useForm";
 
 export const Seacrh = () => {
+  const [values, setValues] = useForm();
+  console.log(values);
   return (
     <label htmlFor="headerSearch">
       <div className={styles.wrap}>
-        <input id="headerSearch" type="text" className={styles.inputSeacrh} />
+        <input
+          id="headerSearch"
+          type="text"
+          name="headerSearch"
+          className={styles.inputSeacrh}
+          value={values.headerSearch || ""}
+          onChange={setValues}
+        />
         <h1>Search</h1>
       </div>
     </label>
---
+++ src/hooks/useForm.tsx
@@ -0,0 +1,23 @@
+import { ChangeEvent, SetStateAction, useState } from "react";
+
+const useForm = () => {
+  interface IUseForm {
+    headerSearch: string;
+  }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [state, setState] = useState<IUseForm & any>({
+    headerSearch: "",
+  });
+
+  const handleChange = (
+    e: SetStateAction<IUseForm> & ChangeEvent<HTMLInputElement>
+  ) => {
+    e.persist();
+    setState((state: IUseForm) => ({
+      ...state,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  return [state, handleChange];
+};
+export default useForm;
